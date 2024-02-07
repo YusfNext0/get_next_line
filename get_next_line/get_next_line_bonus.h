823cae616956cc8706262d1e5c9c967f9777963b
YOUSSEF NACHAT <105310735+YusfNext0@users.noreply.github.com>
2024-02-07 15:39:57 +0100

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynachat <ynachat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 10:21:16 by ynachat           #+#    #+#             */
+/*   Updated: 2024/02/07 15:32:19 by ynachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strxcpy(char *dst, const char *src, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+
+#endif
