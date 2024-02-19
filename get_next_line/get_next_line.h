db406a1032d972707f176955cc9e4249c19c142b
YOUSSEF NACHAT <105310735+YusfNext0@users.noreply.github.com>
2024-02-19 22:57:23 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynachat <ynachat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 22:43:49 by ynachat           #+#    #+#             */
+/*   Updated: 2024/02/17 16:40:26 by ynachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <string.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strxcpy(char *dst, const char *src, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+
+#endif
